63ccaf1d0cbb6eb854e85c4f2929cbe62dece9b8
Shifatur-Rahman <rjnovel28@gmail.com>
2022-07-28 11:31:00 +0600

    function add

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ function buttonClick(event) {
   let inputValue = document.getElementById("mainInput").value;
   if (btnValue == "C") {
     document.getElementById("mainInput").value = "";
+    document.getElementById("secondInput").value = "";
   } else if (btnValue == "DEL") {
     document.getElementById("mainInput").value = inputValue.slice(
       0,
       inputValue.length - 1
     );
   } else if (btnValue == "=") {
-    // document.getElementById("secondInput").value = inputValue;
+    document.getElementById("secondInput").value = inputValue;
     document.getElementById("mainInput").value = eval(inputValue);
   } else {
     document.getElementById("mainInput").value = inputValue + btnValue;
